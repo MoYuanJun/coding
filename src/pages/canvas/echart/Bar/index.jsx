9b5adc9420a9e4b7accb631581c9aa27d66add46
qianyin925 <qianyin95@gmail.com>
2019-10-12 09:08:22 +0800

    refactor: bar 添加坐标点

--- src/pages/canvas/echart/Bar/index.jsx
+++ src/pages/canvas/echart/Bar/index.jsx
@@ -27,11 +27,37 @@ const drawAxis = (canvas) => {
   ctx.strokeStyle = '#fff';
   ctx.stroke();
 
-  // 标点
+  // 坐标点
   const stepLengthY = Math.floor((HEIGHT - 2 * PADDING) / STEP_Y);
   const stepLengthX = Math.floor((WIDTH - 2 * PADDING) / data.length);
+  
+  // x 轴
+  for (let i = 0; i < data.length; i++){
+    const xAxis = data[i].name;
+    const xlen = stepLengthX * (i + 1);
 
+    ctx.moveTo(PADDING + xlen, HEIGHT - PADDING);
+    ctx.lineTo(PADDING + xlen, HEIGHT - PADDING + 5);
+    ctx.stroke();
 
+    ctx.textAlign = 'center';
+    ctx.fillStyle = 'red';
+    ctx.fillText(xAxis, PADDING + xlen - stepLengthX / 2, HEIGHT - PADDING + 15);
+  }
+
+  // Y 轴
+  for (let i = 0; i < STEP_Y; i++){
+    const yAxis = Math.floor(Math.max(...data.map(v => v.value)) / STEP_Y) * (i + 1);
+    const yLen = stepLengthY * (i + 1);
+
+    ctx.moveTo(PADDING, HEIGHT - PADDING - yLen);
+    ctx.lineTo(PADDING - 5, HEIGHT - PADDING - yLen);
+    ctx.stroke();
+
+    ctx.fillStyle = 'red';
+    ctx.textAlign = 'right';
+    ctx.fillText(yAxis, PADDING - 15, HEIGHT - PADDING - yLen + 5);
+  }
 }
 
 const useStateHook = () => {
