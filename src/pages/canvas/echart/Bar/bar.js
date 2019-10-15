60a2670fefe8949e53174ec6a6de87ebedae6833
qianyin925 <qianyin95@gmail.com>
2019-10-15 16:22:03 +0800

    refactor: 分离 bar 代码

--- src/pages/canvas/echart/Bar/bar.js
+++ src/pages/canvas/echart/Bar/bar.js
@@ -1,24 +1,110 @@
 
 export default class Bar {
-  constructor({ width, height, padding }){
+  constructor({ padding, data = [], segsY = 5, divisionLength = 5, barWidth = 40, frameNum = 50 }){
+    this.width = 0;
+    this.height = 0;
     this.ctx = null;
+    this.data = data;
     this.canvas = null;
-    this.width = width;
-    this.height = height;
+    this.segsY = segsY;
     this.padding = padding;
+    this.barWidth = barWidth;
+    this.frameNum = frameNum;
+    this.divisionLength = divisionLength;
+  }
+
+  // 绘制
+  draw = () => {
+    this.drawAxis();
+    this.drawBars();
   }
 
   // 初始化
-  init(container){
-    const canvas = document.createElement('canvas');
-    this.canvas = canvas;
-    this.ctx = canvas.getContext('2d');
+  init = (container) => {
+    const { width, height } = window.getComputedStyle(container);
+    this.canvas = document.createElement('canvas');
+    this.width = parseFloat(width, 10);
+    this.height = parseFloat(height, 10);
+    this.ctx = this.canvas.getContext('2d');
 
-    canvas.width = '100%';
-    canvas.height = '100%';
-    container.appendChild(canvas);
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+    container.appendChild(this.canvas);
   }
 
+  // 绘制坐标轴
+  drawAxis = () => {
+    // 1. 绘制轴线
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.padding, this.padding);
+    this.ctx.lineTo(this.padding, this.height - this.padding);
+    this.ctx.lineTo(this.width - this.padding, this.height - this.padding);
+    this.ctx.strokeStyle = 'red';
+    this.ctx.stroke();
 
+    // 2. 绘制坐标点
+    const stepLengthY = Math.floor((this.height - 2 * this.padding) / this.segsY);
+    const stepLengthX = Math.floor((this.width - 2 * this.padding) / this.data.length);
 
+    // 2.1 Y 轴
+    for(let i = 0; i < this.segsY; i++){
+      const x = this.padding;
+      const y = this.height - this.padding - stepLengthY * (i + 1);
+
+      this.ctx.beginPath();
+      this.ctx.moveTo(x, y);
+      this.ctx.lineTo(x - this.divisionLength, y);
+      this.ctx.strokeStyle = 'red';
+      this.ctx.stroke();
+
+      const value = Math.floor(Math.max(...this.data.map(v => v.value)) / this.segsY) * (i + 1);
+      this.textAlign = 'right';
+      this.ctx.fillText(value, x - 6 * this.divisionLength, y + 4);
+    }
+
+    // 2.2 X 轴
+    for(let i = 0; i < this.data.length; i++){
+      const x =  this.padding + stepLengthX * (i + 1);
+      const y = this.height - this.padding;
+
+      this.ctx.moveTo(x, y);
+      this.ctx.lineTo(x, y + this.divisionLength);
+      this.ctx.strokeStyle = 'red';
+      this.ctx.stroke();
+
+      this.ctx.textAlign = 'center';
+      this.ctx.fillText(this.data[i].name, x - stepLengthX / 2, y + 4 * this.divisionLength);
+    }
+  }
+
+  // 绘制柱状图
+  drawBars = () => {
+    const stepLengthY = Math.floor((this.height - 2 * this.padding) / this.segsY);
+    const stepLengthX = Math.floor((this.width - 2 * this.padding) / this.data.length);
+    for(let i = 0; i < this.data.length; i++){
+      const height = (this.height - 2 * this.padding) / Math.max(...this.data.map(v => v.value)) * this.data[i].value;
+      const x = this.padding + stepLengthX * i + (stepLengthX - this.barWidth) / 2;
+      const y = this.height - this.padding - height;
+      this.drawBar(this.data[i].value, x, y, height);
+    }
+  }
+
+  // 绘制单个柱状图
+  drawBar = (value, x, y, height) => {
+    let i = 1;
+    this.ctx.textAlign = 'center';
+    const draw = () => {
+      this.ctx.clearRect(x, y, this.barWidth, height);
+      const _height = height / this.frameNum * i;
+      
+      this.ctx.fillStyle = 'red';
+      this.ctx.fillRect(x, y + height - _height, this.barWidth, _height);
+      
+      this.ctx.fillStyle = 'blue';
+      this.ctx.fillText(value, x + this.barWidth / 2,  y + height - _height / 2);
+      i += 1;
+      i < this.frameNum && requestAnimationFrame(draw);
+    }
+    requestAnimationFrame(draw);
+  }
 }
--- src/pages/canvas/echart/Bar/index.jsx
+++ src/pages/canvas/echart/Bar/index.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import { useEffect, useRef }  from 'react';
 import Bar from './bar';
 
-const WIDTH = 600;
-const HEIGHT = 500;
-const PADDING = 50;
-const STEP_Y = 5;
-
 const data = [
   { name: '周一', value: 300 },
   { name: '周二', value: 250 },
@@ -15,93 +10,13 @@ const data = [
   { name: '周五', value: 280 },
 ];
 
-// 绘制动画
-const drawBar = (ctx, x, y, w, h) => {
-  const frameNum = 50;
-  let num = 1;
-  const draw = () => {
-    ctx.clearRect(x, y - 5, w, h + 5 );
-
-    ctx.fillStyle = "#1890ff";
-    ctx.textAlign = 'center';
-    ctx.fillRect(x, y + (h - ((h / frameNum) * num)), w, (h / frameNum) * num);
-    ctx.fillText('111', x + w / 2, y + (h - ((h / frameNum) * num)) - 5 );
-
-    num += 1;
-    num <= frameNum && requestAnimationFrame(draw);
-  };
-
-  requestAnimationFrame(draw);
-}
-
-// 1. 绘制坐标轴
-const drawAxis = (canvas) => {
-  const ctx = canvas.getContext('2d');
-  canvas.width = WIDTH;
-  canvas.height = HEIGHT;
-
-  ctx.beginPath();
-  ctx.moveTo(PADDING, PADDING);
-  ctx.lineTo(PADDING, HEIGHT - PADDING);
-  ctx.lineTo(WIDTH - PADDING, HEIGHT - PADDING);
-  ctx.strokeStyle = '#fff';
-  ctx.stroke();
-
-  // 坐标点
-  const stepLengthY = Math.floor((HEIGHT - 2 * PADDING) / STEP_Y);
-  const stepLengthX = Math.floor((WIDTH - 2 * PADDING) / data.length);
-
-  // x 轴
-  for (let i = 0; i < data.length; i++){
-    const xAxis = data[i].name;
-    const xlen = stepLengthX * (i + 1);
-
-    ctx.moveTo(PADDING + xlen, HEIGHT - PADDING);
-    ctx.lineTo(PADDING + xlen, HEIGHT - PADDING + 5);
-    ctx.stroke();
-
-    ctx.textAlign = 'center';
-    ctx.fillStyle = 'red';
-    ctx.fillText(xAxis, PADDING + xlen - stepLengthX / 2, HEIGHT - PADDING + 15);
-  }
-
-  // Y 轴
-  for (let i = 0; i < STEP_Y; i++){
-    const yAxis = Math.floor(Math.max(...data.map(v => v.value)) / STEP_Y) * (i + 1);
-    const yLen = stepLengthY * (i + 1);
-
-    ctx.moveTo(PADDING, HEIGHT - PADDING - yLen);
-    ctx.lineTo(PADDING - 5, HEIGHT - PADDING - yLen);
-    ctx.stroke();
-
-    ctx.fillStyle = 'red';
-    ctx.textAlign = 'right';
-    ctx.fillText(yAxis, PADDING - 15, HEIGHT - PADDING - yLen + 5);
-  }
-
-  // 绘制柱状图
-  // HEIGHT - 2 * PADDING
-  const barWidth = 50;
-  
-  for (let i = 0; i < data.length; i ++ ){
-    const height = ((HEIGHT - 2 * PADDING) / Math.max(...data.map(v => v.value))) * data[i].value;
-
-    drawBar(
-      ctx, 
-      PADDING + stepLengthX * i + ((stepLengthX - barWidth) / 2), 
-      HEIGHT - height - PADDING, 
-      barWidth, 
-      height
-    );
-  }
-}
-
 const useStateHook = () => {
   const containerRef = useRef();
 
   useEffect(() => {
-    const bar = new Bar({});
+    const bar = new Bar({ padding: 50, data });
     bar.init(containerRef.current);
+    bar.draw();
   }, []);
 
   return { containerRef };
@@ -110,8 +25,6 @@ const useStateHook = () => {
 export default (props) => {
   const state = useStateHook(props);
   return (
-    <div ref={state.containerRef} style={{ width: 600, height: 500, background: 'red' }}>
-      
-    </div>
+    <div ref={state.containerRef} style={{ width: 600, height: 500}}></div>
   );
 }
