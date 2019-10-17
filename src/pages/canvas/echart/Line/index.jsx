303cb1af0aa5e38ced6f696d17b5c7fb7eb1dead
qianyin925 <qianyin95@gmail.com>
2019-10-17 09:07:50 +0800

    refactor: 折线图

--- src/pages/canvas/echart/Line/index.jsx
+++ src/pages/canvas/echart/Line/index.jsx
@@ -1,13 +1,22 @@
 import React, { useRef, useEffect } from 'react';
 import Line from './line';
 
+const data = [
+  { name: '周一', value: 300 },
+  { name: '周二', value: 250 },
+  { name: '周三', value: 100 },
+  { name: '周四', value: 220 },
+  { name: '周五', value: 280 },
+];
+
 export default () => {
   const containerRef = useRef(null);
 
 
   useEffect(() => {
-    const line = new Line();
+    const line = new Line({ data });
     line.init(containerRef.current);
+    line.draw();
   }, []);
 
   return (
--- src/pages/canvas/echart/Line/line.js
+++ src/pages/canvas/echart/Line/line.js
@@ -1,29 +1,73 @@
 export default class Line {
-  constructor({ padding = 50, data = [] }){
-    this.canvas = null;
-    this.ctx = null;
+  constructor({ padding = 50, data = [], segsY = 5, divisionLength = 5, barWidth = 40, frameNum = 50  }){
     this.width = 0;
     this.height = 0;
+    this.ctx = null;
     this.data = data;
+    this.canvas = null;
+    this.segsY = segsY;
     this.padding = padding;
+    this.barWidth = barWidth;
+    this.frameNum = frameNum;
+    this.divisionLength = divisionLength;
   }
 
+  // 绘制
+  draw = () => {
+    this.drawAxis();
+  }
 
   // 初始化
   init = (container) => {
     const { width, height } = window.getComputedStyle(container);
-    this.width = width;
-    this.height = height;
+    this.width = parseFloat(width);
+    this.height = parseFloat(height);
     this.canvas = document.createElement('canvas');
-    this.canvas.width = parseFloat(width);
-    this.canvas.height = parseFloat(height);
-    this.canvas.style.background = 'red';
+
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
     this.ctx = this.canvas.getContext('2d');
     container.appendChild(this.canvas);
   }
 
+
   // 绘制坐标轴
-  // drwxz
+  drawAxis = () => {
+    // 坐标轴线
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.padding, this.padding);
+    this.ctx.lineTo(this.padding, this.height - this.padding);
+    this.ctx.lineTo(this.width - this.padding, this.height - this.padding);
+    this.ctx.strokeStyle = 'red';
+    this.ctx.stroke();
+
+    const stepLengthX = Math.floor((this.width - 2 * this.padding) / this.data.length);
+    const stepLengthY = Math.floor((this.height - 2 * this.padding) / this.segsY);
+    
+    // x - 分割点
+    for(let i = 0; i < this.data.length; i++){
+      const y = this.height - this.padding;
+      const x = this.padding + stepLengthX * (i + 1);
+      
+      // 分割线
+      this.ctx.beginPath();
+      this.ctx.moveTo(x, y);
+      this.ctx.lineTo(x, y + this.divisionLength);
+      this.ctx.stroke();
+
+    }
+
+    // y - 分割点
+    for(let i = 0; i < this.segsY; i++){
+      const x = this.padding;
+      const y = this.height - this.padding - stepLengthY * (i + 1);
+      this.ctx.beginPath();
+      this.ctx.moveTo(x, y);
+      this.ctx.lineTo(x - this.divisionLength, y);
+      this.ctx.stroke();
+    }
+
+  }
 
 
 
