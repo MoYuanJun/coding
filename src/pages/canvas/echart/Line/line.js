353351696c4b7a386c90b5371dcc26782015273a
qianyin925 <qianyin95@gmail.com>
2019-10-21 09:06:38 +0800

    refactor: 折线图修改

--- src/pages/canvas/echart/Line/line.js
+++ src/pages/canvas/echart/Line/line.js
@@ -9,12 +9,13 @@ export default class Line {
     this.padding = padding;
     this.barWidth = barWidth;
     this.frameNum = frameNum;
-    this.divisionLength = divisionLength;
+    this.divisionLength = divisionLength;  // 坐标轴上分段线长度
   }
 
   // 绘制
   draw = () => {
     this.drawAxis();
+    this.drawLines();
   }
 
   // 初始化
@@ -55,20 +56,44 @@ export default class Line {
       this.ctx.lineTo(x, y + this.divisionLength);
       this.ctx.stroke();
 
+      this.ctx.textAlign = 'center';
+      this.ctx.fillText(this.data[i].name, x - stepLengthX / 2, y + 4 * this.divisionLength);
     }
 
     // y - 分割点
     for(let i = 0; i < this.segsY; i++){
       const x = this.padding;
       const y = this.height - this.padding - stepLengthY * (i + 1);
+
+      // 分割点
       this.ctx.beginPath();
       this.ctx.moveTo(x, y);
       this.ctx.lineTo(x - this.divisionLength, y);
       this.ctx.stroke();
-    }
 
+      const max = Math.max(...this.data.map(v => v.value));
+      const value = max / this.segsY * (i + 1);
+      const yHeight = (this.height - 2 * this.padding) / max * value;
+      
+      this.ctx.fillText(
+        Math.floor(value), 
+        Math.floor(x - 4 * this.divisionLength), 
+        Math.floor(this.height - this.padding - yHeight + 4)
+      );
+    }
   }
 
+  // 绘制折线图
+  drawLines = () => {
+    this.ctx.beginPath();
+    for (let i = 0; i < this.data.length; i++){
+      const x = Math.floor((this.width - 2 * this.padding) / this.data.length * (i + 0.5)) + this.padding;
+      const yLength = Math.floor((this.width - 2 * this.padding) / Math.max(...this.data.map(v => v.value)) * this.data[i].value);
 
+      this.ctx.lineTo(x, y);
+
+    } 
+    this.ctx.stroke();
+  }
 
 }
