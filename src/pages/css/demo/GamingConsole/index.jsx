fd1828cfb2a9f357bab6bb874b0a9d6567076add
qianyin925 <qianyin95@gmail.com>
2020-06-02 11:21:22 +0800

    refactor: css 游戏机

--- src/pages/css/demo/GamingConsole/index.jsx
+++ src/pages/css/demo/GamingConsole/index.jsx
@@ -5,7 +5,18 @@ import scss from './index.module.scss';
 export default () => (
   <div className={scss.body}>
     <div className={scss['gaming-console']}>
-      游戏机
+      <div className={scss.screen}/>
+
+      <div className={scss['btns-a-b']}>
+        <div className={scss.btn}>A</div>
+        {/* <div className={scss.btn}>B</div> */}
+      </div>
+
+      <div className={scss.screw}>
+        <div className={scss['screw-item']}/>
+        <div className={scss['screw-item']}/>
+        <div className={scss['screw-item']}/>
+      </div>
     </div>
   </div>
 );
