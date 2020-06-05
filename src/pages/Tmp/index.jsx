b855f02a2dd15450635b251cf7c1540e9eaf17fe
qianyin925 <qianyin95@gmail.com>
2020-06-05 16:48:32 +0800

    refactor: svg　研究学习

--- src/pages/Tmp/index.jsx
+++ src/pages/Tmp/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
-const done = () => {
-
-};
+import scss from './index.module.scss';
 
 export default () => (
-  <div>
-    {done()}
+  <div className={scss.body}>
+    <svg xmlns="qy" version="1.1">
+      <a xlinkHref="http://www.w3schools.com/svg/" target="_blank">
+        <text x="0" y="15" fill="red">i love svg</text>
+      </a>
+    </svg>
   </div>
 );
