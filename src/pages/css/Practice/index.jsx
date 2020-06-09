69a2ed905860ee71e955b6f242f90ea0b416bb62
qianyin925 <qianyin95@gmail.com>
2020-06-09 10:48:26 +0800

    refactor: css 练习 - mask

--- src/pages/css/Practice/index.jsx
+++ src/pages/css/Practice/index.jsx
@@ -1,7 +1,10 @@
+// https://blog.csdn.net/qq_44607694/article/details/90551133
 import React from 'react';
+import img from './img/img.jpg';
+import scss from './index.module.scss';
 
 export default () => (
-  <div>
-    练习
+  <div className={scss.body}>
+    <img src={img} className={scss.mask}/>
   </div>
 );
