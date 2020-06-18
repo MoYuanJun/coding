2a89c11b573673c7b96f4b72d8993ccb32dc96b0
qianyin925 <qianyin95@gmail.com>
2020-06-18 10:53:33 +0800

    refactor: css 选项卡交互 - 推拉

--- src/pages/css/TabBarInteraction/PushPull/index.jsx
+++ src/pages/css/TabBarInteraction/PushPull/index.jsx
@@ -1,5 +1,6 @@
 // https://codepen.io/Anna_Batura/details/qBbaXBd
 import React from 'react';
+import classNames from 'classnames';
 import scss from './index.module.scss';
 import {
   AlignLeftOutlined,
@@ -11,26 +12,59 @@ import {
 const MENU_LIST = [
   {
     icon: <AlignLeftOutlined />,
-    title: '',
+    title: 'MENU',
   },
   {
     icon: <WechatOutlined />,
-    title: '',
+    title: 'USERS',
   },
   {
     icon: <TeamOutlined />,
-    title: '',
+    title: 'CHAT',
   },
   {
     icon: <SettingOutlined />,
-    title: '',
+    title: 'EDIT',
   },
 ];
 
-export default () => (
-  <div className={scss.body}>
-    <div className={scss.menu}>
-      推拉
+const useStateHook = () => {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const onChange = activeIndex => {
+    setActiveIndex(activeIndex);
+  };
+
+  return { onChange, activeIndex };
+};
+
+export default () => {
+  const state = useStateHook();
+
+  return (
+    <div className={scss.body}>
+      <div className={scss.menu}>
+        {MENU_LIST.map((v, index) => (
+          <div
+            key={index}
+            className={classNames(
+              scss.item,
+              { [scss.active]: state.activeIndex === index }
+            )}
+            onClick={state.onChange.bind(null, index)}>
+            <div className={scss.icon}>
+              {v.icon}
+              {index === 0 ?
+                <div className={scss.tip}>
+                  About Us <br/>
+                  Contact Us
+                </div> : null
+              }
+            </div>
+            <div className={scss.title}>{v.title}</div>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
