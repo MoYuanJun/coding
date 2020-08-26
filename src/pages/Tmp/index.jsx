938cdac08c35304be843519aed8d192b43e039ba
qianyin925 <qianyin925@qq.com>
2020-08-26 09:40:05 +0800

    refactor: demo 修改

--- src/pages/Tmp/index.jsx
+++ src/pages/Tmp/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import scss from './index.module.scss';
 // https://juejin.im/post/6862156294611009544
-export default () => (
-  <div className={scss.body}>
-    <div className={scss.emphasis}>
-      添加修饰符
+
+const useStateHook = () => {
+  const onChange = event => {
+    const [file] = event.target.files;
+    console.log(file);
+  };
+
+  return { onChange };
+};
+
+export default () => {
+  const state = useStateHook();
+
+  return (
+    <div className={scss.body}>
+      <input type="file" onChange={state.onChange}/>
     </div>
-  </div>
-);
+  );
+};
